99dc85ac026a26e5752c9485988a798c2c264275
wedi <code@dirk-weise.de>
2014-09-12 06:06:32 +0200

    Prepare change to theme directory in 'content' folder

--- content/mu-plugins/register-theme-directory.php
+++ content/mu-plugins/register-theme-directory.php
@@ -1,3 +1,7 @@
 <?php
 
+// WordPress' original theme directory containing the shipped themes
 register_theme_directory( ABSPATH . 'wp-content/themes/' );
+
+// new theme directory
+// register_theme_directory( WP_CONTENT_DIR . '/themes/' );
