4bc1a1aa39966057f2d993976ea78998f18c2ef7
wedi <code@dirk-weise.de>
2014-09-12 05:15:16 +0200

    Add debugging and direct file access options to local config

--- local-config-sample.php
+++ local-config-sample.php
@@ -10,3 +10,20 @@ define( 'DB_NAME', 'local_db_name' );
 define( 'DB_USER', 'local_db_user' );
 define( 'DB_PASSWORD', 'local_db_password' );
 define( 'DB_HOST', 'localhost' ); // Probably 'localhost'
+
+
+/* Debug settings */
+define( 'WP_DEBUG', false );
+
+if ( WP_DEBUG ) {
+	@ini_set( 'log_errors', 'Off' );
+	@ini_set( 'display_errors', 'On' );
+	@ini_set( 'error_reporting', E_ALL );
+	define( 'WP_DEBUG_LOG', false );
+	define( 'WP_DEBUG_DISPLAY', true );
+}
+
+define( 'FS_METHOD', 'direct');
+
+define( 'FS_CHMOD_DIR', ( 02775 ) );
+define( 'FS_CHMOD_FILE', ( 0664 ) );
